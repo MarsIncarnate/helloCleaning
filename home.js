5b4e89dda53a7450468c24a4f722b952070a7d92
MarsIncarnate <marvellousadeogun@gmail.com>
2024-01-11 12:00:00 +0100

    Update homepage code

--- home.js
+++ home.js
@@ -1,163 +1,110 @@
-import wixWindowFrontend from 'wix-window-frontend';
-import wixLocation from 'wix-location'
+import wixLocation from 'wix-location';
 
 $w.onReady(function () {
-	
-	function openOptions(parent, box) {
-		$w(parent).onClick(() => {
-			if ($w(box).hidden) {
-				$w(box).show()
-			}
-			else {
-				$w(box).hide()
-			}
-		})
-	}
-	let bedroom = 0.00;
-	let bathroom = 5.00;
-	let recurrence = 0.00;
+    let bedroom = 0.00;
+    let bathroom = 5.00;
+    let recurrence = 0.00;
+    let selectedData = { 'bedroom': '', 'bathroom': '', 'frequency': '', 'type': '', 'price': '' }
 
-	function handleClick(text, parentText, parent) {
-		$w(text).onClick(() => {
-			let textVal = $w(text).text
-			console.log(textVal)
-			$w(parentText).text = textVal
-			$w(parent).hide()
-			if (parentText == '#text77') {
-				let check;
-				//let standard = 0;
-				if (check != 'studio' && text == '#text76') {
-					check = 'studio'
-					bedroom = 0.00
-				}
-				else if (check != '1' && text == '#text87') {
-					// let num = $w('#text118').text;
-					// let res = +(num) + 5;
-					// $w('#text118').text = res.toString()
-					check = '1'
-					bedroom = 5.00
-				}
-				else if (check != '2' && text == '#text88') {
-					check = '2'
-					bedroom = 25.00
-				}
-				else if (check != '3' && text == '#text89') {
-					check = '3'
-					bedroom = 45.00
-				}
-				else if (check != '4' && text == '#text90') {
-					check = '4'
-					bedroom = 60.00
-				}
-				else if (check != '5' && text == '#text91') {
-					check = '5'
-					bedroom = 85.00
-				}
-				else if (check != '6' && text == '#text93') {
-					check = '6'
-					bedroom = 115.00
-				}
-				else if (check != '7' && text == '#text92') {
-					check = '7'
-					bedroom = 135.00
-				}
-			}
-			if (parentText == '#text102' ) {
-				let bathCheck;
-				if (bathCheck != '1' && text == '#text101') {
-					bathroom = 5.00
-					bathCheck = '1'
-				}
-				else if (bathCheck != '2' && text == '#text100') {
-					bathroom = 20.00
-					bathCheck = '2'
-				}
-				else if (bathCheck != '3' && text == '#text98') {
-					bathroom = 45.00
-					bathCheck = '3'
-				}
-				else if (bathCheck != '4' && text == '#text99') {
-					bathroom = 65.00
-					bathCheck = '4'
-				}
-				else if (bathCheck != '5' && text == '#text96') {
-					bathroom = 80.00
-					bathCheck = '5'
-				}
-				else if (bathCheck != '6' && text == '#text97') {
-					bathroom = 90.00
-					bathCheck = '6'
-				}
-			}
-			if (parentText == '#text111') {
-				let recCheck;
-				//let standard = 0;
-				// if (recCheck != 'rec' && text == '#text110') {
-				// 	recCheck = 'rec'
-				// 	recurrence = 0.00
-				// }
-				if (recCheck != '1time' && text == '#text109') {
-					recurrence = 165.00
-					recCheck = '1time'
-				}
-				else if (recCheck != 'weekly' && text == '#text107') {
-					recurrence = 90.00
-					recCheck = 'weekly'
-				}
-				else if (recCheck != '2weeks' && text == '#text108') {
-					recurrence = 110.00
-					recCheck = '2weeks'
-				}
-				else if (recCheck != '4weeks' && text == '#text106') {
-					recurrence = 130.00
-					recCheck = '4weeks'
-				}
-			}
-			let price = bedroom + bathroom + recurrence
-			$w('#text118').text = '$' + price.toString() + '.00'
-		})
-	}
-	openOptions('#box10', '#box11');
-	openOptions('#box17', '#box16');
-	openOptions('#box49', '#box48')
-	openOptions('#box60', '#box59')
-	openOptions('#box62', '#box69')
-	handleClick('#text74', '#text70', '#box11')
-	handleClick('#text73', '#text70', '#box11')
-	handleClick('#text76', '#text77', '#box16')
-	for (let i=87; i<=93; i++) {
-		handleClick('#text'+i, '#text77', '#box16')
-	}
-	for (let i=101; i>=96; i--) {
-		handleClick('#text'+i, '#text102', '#box48')
-	}
-	for (let i=109; i>=106; i--) {
-		handleClick('#text'+i, '#text111', '#box59')
-	}
-	for (let i=117; i>=115; i--) {
-		handleClick('#text'+i, '#text112', '#box69')
-	}
+    const dropdown1 = $w('#dropdown1');
+    dropdown1.onChange((event) => {
+        let value = event.target.value
+        selectedData.bedroom = value
+        let check;
+        if (check != 'studio' && value == 'Studio') {
+            check = 'studio'
+            bedroom = 0.00
+        }
+        else if (check != '1' && value == '1 Bedroom') {
+            check = '1'
+            bedroom = 5.00
+        }
+        else if (check != '2' && value == '2 Bedrooms') {
+            check = '2'
+            bedroom = 25.00
+        }
+        else if (check != '3' && value == '3 Bedrooms') {
+            check = '3'
+            bedroom = 45.00
+        }
+        else if (check != '4' && value == '4 Bedrooms') {
+            check = '4'
+            bedroom = 60.00
+        }
+        else if (check != '5' && value == '5 Bedrooms') {
+            check = '5'
+            bedroom = 85.00
+        }
+        else if (check != '6' && value == '6 Bedrooms') {
+            check = '6'
+            bedroom = 115.00
+        }
+        else if (check != '7' && value == '7 Bedrooms') {
+            check = '7'
+            bedroom = 135.00
+        }
+        let price = bedroom + bathroom + recurrence
+        $w('#text118').text = '$' + price.toString() + '.00'
+    });
 
+    const dropdown2 = $w('#dropdown2')
+    dropdown2.onChange((event) => {
+        let value = event.target.value
+        selectedData.bathroom = value
+        let bathCheck;
+        if (bathCheck != '1' && value == '1 Bathroom') {
+            bathroom = 5.00
+            bathCheck = '1'
+        } else if (bathCheck != '2' && value == '2 Bathrooms') {
+            bathroom = 20.00
+            bathCheck = '2'
+        } else if (bathCheck != '3' && value == '3 Bathrooms') {
+            bathroom = 45.00
+            bathCheck = '3'
+        } else if (bathCheck != '4' && value == '4 Bathrooms') {
+            bathroom = 65.00
+            bathCheck = '4'
+        } else if (bathCheck != '5' && value == '5 Bathrooms') {
+            bathroom = 80.00
+            bathCheck = '5'
+        } else if (bathCheck != '6' && value == '6 Bathrooms') {
+            bathroom = 90.00
+            bathCheck = '6'
+        }
+        let price = bedroom + bathroom + recurrence
+        $w('#text118').text = '$' + price.toString() + '.00'
+    })
+
+    const dropdown3 = $w('#dropdown3')
+    dropdown3.onChange((event) => {
+        let value = event.target.value
+        selectedData.frequency = value
+        let recCheck
+        if (recCheck != '1time' && value == 'One Time Deep Clean') {
+            recurrence = 165.00
+            recCheck = '1time'
+        } else if (recCheck != 'weekly' && value == 'Weekly') {
+            recurrence = 90.00
+            recCheck = 'weekly'
+        } else if (recCheck != '2weeks' && value == 'Every 2 Weeks') {
+            recurrence = 110.00
+            recCheck = '2weeks'
+        } else if (recCheck != '4weeks' && value == 'Every 4 Weeks') {
+            recurrence = 130.00
+            recCheck = '4weeks'
+        }
+        let price = bedroom + bathroom + recurrence
+        let priceTxt =  '$' + price.toString() + '.00'
+        $w('#text118').text = priceTxt
+        selectedData.price = priceTxt
+    })
+    const dropdown5 = $w('#dropdown5')
+    dropdown5.onChange((event) => {
+        selectedData.type = event.target.value
+    })
+
+    $w('#button63').onClick(() => {
+		let data = JSON.stringify(selectedData)
+		wixLocation.to(`/quote?selectedData=${encodeURIComponent(data)}`)
+	})
 });
-
-
-export function box9_mouseOut(event) {
-	if (wixWindowFrontend.formFactor == 'Desktop') {
-		$w('#box11').hide()
-	}
-}
-export function box50_mouseOut(event) {
-	if (wixWindowFrontend.formFactor == 'Desktop') {
-		$w('#box48').hide()
-	}
-}
-export function box61_mouseOut(event) {
-	if (wixWindowFrontend.formFactor == 'Desktop') {
-		$w('#box59').hide()
-	}
-}
-export function box18_mouseOut(event) {
-	if (wixWindowFrontend.formFactor == 'Desktop') {
-		$w('#box16').hide()
-	}
-}
